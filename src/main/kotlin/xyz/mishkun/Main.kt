dd5218fa5e0ec7563574e381ca427064896c0c00
themishkun <mishkun.lev@gmail.com>
2023-11-06 20:16:05 +0100

    Fix detekt

--- src/main/kotlin/xyz/mishkun/Main.kt
+++ src/main/kotlin/xyz/mishkun/Main.kt
@@ -1,20 +1,5 @@
 package xyz.mishkun
 
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.types.file
-import xyz.mishkun.parser.FileTree
-import xyz.mishkun.views.PageTraverser
-
-class Generator : CliktCommand() {
-    val sourcesDir by argument(help = "Directory with markdown files").file(mustExist = true, mustBeReadable = true)
-    val targetDir by argument(help = "Directory where to put generated html files").file()
-    override fun run() {
-        echo("Generating site from $sourcesDir to $targetDir")
-        FileTree(sourcesDir, targetDir, PageTraverser()).walk()
-    }
-}
-
 fun main(args: Array<String>) {
     Generator().main(args)
 }
